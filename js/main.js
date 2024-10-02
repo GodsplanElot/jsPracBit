d1bc448c6229acfcb1985a7d3b82183a66731232
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-02 12:24:33 +0100

    added nested if statement to the structure

--- js/main.js
+++ js/main.js
@@ -6,9 +6,30 @@ console.log(myName.charAt(Math.floor(Math.random() * myName.length))) */
 
 /* Stops here */
 
-
+/*
 if (condition) {
-    // run some code
+    // run some code;
+} else if {
+    //run some other code;
 } else {
-    //run some other code
+    if (condition) {
+        //run nested code;
+    } else {
+        run other nested code; 
+    }
 }
+    
+
+*/
+
+
+let soup = "Chicken noodle soup"
+let reply;
+
+if (soup) {
+    reply = `Here's your order of ${soup}`;
+}else {
+    reply = `Sorry, we're out of ${soup}`;
+}
+
+console.log(reply);
