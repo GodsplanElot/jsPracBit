cb033ed05b6cc44b1ad42d70c6d2835b15feb095
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-06 11:39:35 +0100

    updated the js files, created a js file for illustrataion

--- js/promptGame.js
+++ js/promptGame.js
@@ -5,9 +5,18 @@ if (playGame) {
     let playerChoice = prompt("please enter rock,paper, or scissors.");
     if (playerChioce) {
         //Then we continue to play⚡☣⚡
+        let playerOne = playerChoice.trim().toLowerCase();
+        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+            //Then we continue with the game logic
+
+            let computerChioce = Math.floor(Math.random() * 3 + 1);
+
+        } else {
+            alert ("you didn't enter rock, paper, or scissore.");
+        }
     } else {
-        alert("i guess you changed your mind")
+        alert("i guess you changed your mind");
     }
 } else{
-    alert("okay, maybe next time.")
+    alert("okay, maybe next time.");
 }
