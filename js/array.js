1daf17a486862d34ef5d00bcaa74e2e6c27df05e
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-07 21:30:52 +0100

    updated the array logic

--- js/array.js
+++ js/array.js
@@ -1,4 +1,17 @@
 //ARRAYS
 //Syntax
-const myArray = [];
+
+//const myArray = [];
+// This is the basic syntax of an array it takes in a variable and is asigned to a list covered in the square parenthesis
+
+//values can are stored in the array like so
+/*
+myArray[0] = "Dave";
+myArray[1] = 1001;
+myArray[2] = false; 
+*/
+
+//we can log out the array with the console.log function or method like so
+//console.log(myArray)
+
 
