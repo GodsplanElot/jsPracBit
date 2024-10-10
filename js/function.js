707d465c64839cebc0a695d0164daeaee1c81c13
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-10 08:48:16 +0100

    updated the function structure

---
+++ js/function.js
@@ -0,0 +1,20 @@
+//This is how we declare a function or create a function
+/* function name(params) {
+    // return function LOgic ☣;
+} */
+
+//how the function is used
+//.name(param)
+
+//There are different types of function structure
+
+//The nameles function, the function is stored in the variable like so
+/* const myVar = function (params) {
+    //return function logic ☣;
+} */
+
+
+//How the function is used
+//.myVar(param)
+
+
