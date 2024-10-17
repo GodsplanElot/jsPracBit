216a6ef063962f8c6c0e3ba95dfe830d1edc383f
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-17 13:04:59 +0100

    updated the object syntax, started creation of car object

--- js/object.js
+++ js/object.js
@@ -10,18 +10,39 @@ console.log(myObj); */
 
 /* objects can store more than just name, this is an example of different datatypes an oject can store */
 
-const anotherObj = { 
+/* const anotherObj = { 
     alive : true,
     answer: 42,
     hobbies: ["eat", "sleep", "code"],
     beverage: {
         morning: "coffee",
-        afternoon: "iced tea"
+        afternoon: "iced tea",
+    },
+    action: function() {
+        /* return "hello world!"; */
+        return `Time for ${this.beverage.morning}`;
     }
-}
+} */
 
 //objects can store even anothr object inside it and it is shown above, this is how the object key value pair are accessed and sent to the console.
 
-console.log(anotherObj.beverage.morning)
+/* console.log(anotherObj.beverage.morning); */
 
-//This method is called object chaining
+//This method is called object chaining it is used to access objects within object, to access values in an array, do like so.
+
+/* console.log(anotherObj.hobbies[0]); */
+
+//This will return the first value of the array and log it to the console, you can also use square bracket to acces the key value pair of an object like so below
+
+/* console.log(anotherObj ["alive"]); */
+
+
+// you can also store a method which is a function insied an object where the fucntion command syntax is stored in the vlaue and this can be any type of logic type, be it "if Statments, Switch Statements, Ternary Operator stament ", this can be seen above in the example and can be used like so below.
+
+/* console.log(anotherObj.action()); */
+
+//Now with the template literal you can refer to other items datatypes stored within the object like so above and you can log it in the console like so below
+
+/* console.log(anotherObj.action()); */
+
+
