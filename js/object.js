1a08ec4db8fd836f7ddb1700b4bc8c919f17d4e4
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-17 12:15:20 +0100

    updated the object syntax

--- js/object.js
+++ js/object.js
@@ -1,4 +1,23 @@
 //This is the simple syntax of object
 // what you should know about objects, it comes in a "key: 'Value'" pair
-const myObj = { name: "Elot"};
-console.log(myObj);
+/* const myObj = { name: "Elot"};
+console.log(myObj); */
+
+/* This will return date in a format that displays bot the key value pair but if you use the dot notation to chain what key you want to the main object name you will get the value alone printed out in the console. example below */
+
+/* console.log(myObj.name) */
+
+
+/* objects can store more than just name, this is an example of different datatypes an oject can store */
+
+const anotherObj = { 
+    alive : true,
+    answer: 42,
+    hobbies: ["eat", "sleep", "code"],
+    beverage: {
+        morning: "coffee",
+        afternoon: "iced tea"
+    }
+}
+
+//objects can store even anothr object inside it and it is shown above, this is how the object key value pair are accessed and sent to the console.
