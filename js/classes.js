5b08733c09f2c0f4373459d5df3ef5c605ffc9c7
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-17 14:22:10 +0100

    finished all the syntax in classes

---
+++ js/classes.js
@@ -0,0 +1,17 @@
+//Javascript class is used as a constructor for creating objects. for example lets create a constructor for making pizza
+
+//basic class syntax
+/* class Pizza {
+    constructor() {
+        this.size = "medium";
+        this.crust = "orginal";
+    }
+    bake () {
+        console.log(`Baking a ${this.size} ${this.crust} crust pizza`)
+    }
+} */
+
+// Now we use all this above as a constructor to create objects, e.g below.
+
+/* const myPizza = new Pizza ()
+myPizza.bake() */
