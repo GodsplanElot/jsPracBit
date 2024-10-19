3072decbbfd307cc41bd4869b6c303bece134e84
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-20 00:29:36 +0100

    finished error handling, Started Event Listiners

---
+++ js/errorHandling.js
@@ -0,0 +1,12 @@
+//Error Handling
+//Its basicaly how to handle or manage errors in javascript, the syntax is below
+//There are different types of erros in javascript
+/*
+1: Reference Error.
+        This can be caught and handled or managed
+    Syntax:-(What it looks like) 
+    
+2: Syntax Error
+    This wil stop your code completely form excuting
+
+*/
