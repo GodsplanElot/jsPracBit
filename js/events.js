e76e9fd94de04b2a803696c880024e278062098f
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-20 20:21:41 +0100

    updated the event syntax

--- js/events.js
+++ js/events.js
@@ -1,6 +1,7 @@
 //Event and Responding to events.
 //before you can interact with the DOM, you have to make sure your DOM is fylly loaded
 
-const view = document.querySelector("#view2");
-console.log(view);
+//Syntax for adding an eventlistener
+// .addEventListener("event (e.g click)", function, useCapture)
 
+//This is the basic syntax of an event listener, you listen or wait for an event to happen
