809ac979a3d5d1e86885e9f6c2594bd566e20c3d
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-20 20:02:48 +0100

    created html page to view my results, created a custom css to manage the style

--- js/events.js
+++ js/events.js
@@ -1,2 +1,6 @@
-//Event and Reponding to events.
+//Event and Responding to events.
 //before you can interact with the DOM, you have to make sure your DOM is fylly loaded
+
+const view = document.querySelector("#view2");
+console.log(view);
+
