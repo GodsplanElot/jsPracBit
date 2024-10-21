20ae8e27409086f336b6445b6c6296d396da998a
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-21 20:38:43 +0100

    updated the fetch js

---
+++ js/fetchAPI.js
@@ -0,0 +1,7 @@
+//Fetch API requires a discussion of...
+// Callbacks, Promises, Thenables, and Async/Await, we are going to collectively break it down individualy.
+
+//Callbacks.
+/*
+This are basically funtions that are passed on to other functions as arguments or parameters and the function will be called when there are done with other processes
+*/
