a6d3e4e71085ff55a953b879e6937e1b27dbc3f0
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-22 03:22:52 +0100

    updated the syntax

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -4,4 +4,10 @@
 //Callbacks.
 /*
 This are basically funtions that are passed on to other functions as arguments or parameters and the function will be called when there are done with other processes
+
+SYNTAX
+function firstFunction(parameters, callback) {
+    // do stuff
+    callback();
+}
 */
